842889c00bfc9cb38ad5dee6b594d2aeb39ed918
jungdahae1225 <jungdahae1225@gmail.com>
2021-08-17 23:58:56 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data","hello!!!");
+        return "hello"; //thymeleaf viewName매핑 : 화면이름 templates에 있는 hello.html로 이동. (.html은 자동으로 붙는다.)
+
+    }
+}
