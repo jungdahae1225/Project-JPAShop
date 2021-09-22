054b59bb7f4c87b9f8e21bb97efed7d6bab0d332
jungdahae1225 <jungdahae1225@gmail.com>
2021-09-22 16:57:16 +0900

    API_dev

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,22 +2,53 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //기본적으로 api와 화면 구성 코드들은 공통처리 부분이 상이하기 때문에 패키지 단위로 분리 해주는 것이 좋다
 @RestController //@Controller@ResponseBody 이 둘을 합친 어노테이션- @ResponseBody는 데이터를 바로 json이나 xml로 보내고자 할때 씀.
 @RequiredArgsConstructor
 public class MemberApiController {
+
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> memberV1(){ //엔티티를 직접 노출하는 방법(@JsonIgnore 어노테이션으로 해결. -> 궁극적인 해결책이 아니고, 기타 다른 문제를 야기한다.)
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){ //엔티티를 직접 노출하는 방법(@JsonIgnore 어노테이션으로 해결. -> 궁극적인 해결책이 아니고, 기타 다른 문제를 야기한다.)
+        List<Member> findMembers = memberService.findMembers();
+
+        /**..JAVA8문법..**/
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name; //단순하게 name만 넘겨 받는 api를 원한다고 가정하자. (body에 name만 노출시키기)
+    }
+
+    /***
+     * API를 만들 때 스펙에 맞는 DTO를 따로 만들고 활용할 것.
+     * */
+    @Data
+    @AllArgsConstructor
+    static class Result<T> { //제네릭으로 작성하여 body의 데이터에 대한 유연성을 살릴 수 있다.
+        private T data;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { //json으로 온 body를 파라미터인 member에 넣어준다.
@@ -34,6 +65,31 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberResponseV2(
+            @PathVariable("id")Long id,
+            @RequestBody @Valid UpdateMemberRequest request){
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(),findMember.getName());
+
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    /***
+     * API를 만들 때 스펙에 맞는 DTO를 따로 만들고 활용할 것.
+     * */
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
     @Data
     static class CreateMemberRequest {
         private String name;
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,6 +24,8 @@ public class Member {
     @Embedded
     private Address address;
 
+    //@JsonIgnore // 엔티티를 직접 넘길 때 공개 되지 않아야 하는 정보를 뺄 수 있도록 도와준다. -> body에 Order 정보를 뺄 수 있다.
+    //그러나 쓰지 않는 것이 좋다. Member엔티티를 사용하는 곳이 하나인 경우는 적기 때문에 오더 정보가 필요한 클라이언트에서는 이 어노테이션을 쓰면 안되고 어쩌고 저쩌고 하여튼 복잡해짐.
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>(); //컬렉션은 이렇게 필드에서 이렇게 바로 초기화 하자.
 }
